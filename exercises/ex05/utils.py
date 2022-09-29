9bede4e68d03381f4004210acf62e7d51645c288
Dylan Shukla <ddshukla@ad.unc.edu>
2022-09-28 21:01:59 -0400

    utils

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,40 @@
+"""Utils file for function definitons."""
+
+__author__: str = "730577405"
+
+
+def only_evens(x: list[int]) -> list[int]:
+    """Returns all even in list."""
+    i: int = 0
+    y: list[int] = []
+    while i < len(x):
+        if x[i] % 2 == 0:
+            y.append(x[i])
+        i+= 1
+    return y
+
+
+def concat(x: list[str], y: list[str]) -> list[str]:
+    """Combines two lists."""
+    z: list[str] = []
+    z.append(x)
+    z.append(y)
+    return z
+
+
+def sub(x: list[int], si: int, ei: int) -> list[int]:
+    """Returns values of list within given intervals."""
+    i: int = 0
+    ei = len(x) - 1
+    y: list[int] = []
+    if si < 0:
+        si = i
+    if ei > len(x):
+        ei = len(x) - 1
+    if len(x) == 0 or si > len(x) or ei <= 0:
+        return y
+    while i < len(x):
+        if i >= si and i < ei:
+            y.append(x[i])
+        i += 1
+    return y
---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,51 @@
+"""Test file for utils."""
+
+__author__: str = "730577405"
+
+
+from exercises.ex05.utils import only_evens, sub, concat
+
+
+def test_only_evens_edge() -> None:
+    """Edge case for only_evens."""
+    assert(list[int]) == []
+
+
+def test_only_evens_1() -> None:
+    """First use case for only_evens."""
+    assert([2, 5, 7, 8]) == [2, 8]
+
+
+def test_only_evens_2() -> None:
+    """Second use case to test only_evens."""
+    assert([4, 10, 12]) == [4, 10, 12]
+
+
+def test_sub_edge() -> None:
+    """Edge case for sub."""
+    assert([], []) == []
+
+
+def test_sub_1() -> None:
+    """First use case for sub."""
+    assert([1, 2, 3], [4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+
+
+def test_sub_2() -> None:
+    """Second use case for testing sub function."""
+    assert([4, 3, 1], [2, 6]) == [4, 3, 1, 2, 6]
+
+
+def concat_edge() -> None:
+    """Edge case for concat."""
+    assert([10, 20, 30, 40], -1, 3) == [10, 20, 30]
+
+
+def concat_1() -> None:
+    """Use case for concat testing."""
+    assert([5, 10, 15], 1, 2) == [10]
+
+
+def concat_2() -> None:
+    """Second use case for concat testing."""
+    assert([4, 7, 8, 1, 3], 1, 4) == [7, 8, 1]
